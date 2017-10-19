52f79963a540c022dbbd6c6685e1f7bf274b09b3
liuzhaohong <liuzhaohong@nxin.com>
2017-10-19 18:20:08 +0800

    初次提交--springboot gradlle 配置

---
+++ common/src/main/java/com/nxin/etpweb/SpringBootStartApplication.java
@@ -0,0 +1,15 @@
+package com.nxin.etpweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by zhaohong on 2017/10/18.
+ */
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
---
+++ spring-boot/src/main/java/com/nxin/etpweb/Application.java
@@ -0,0 +1,21 @@
+package com.nxin.etpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by zhaohong on 2017/10/18.
+ */
+@SpringBootApplication
+@ImportResource({"classpath:velocity.xml"})
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+    }
+}
