fdc45b1d40828a9e81fec36d901db1b42c7efcd7
liuzhaohong <liuzhaohong@nxin.com>
2018-04-28 16:03:41 +0800

    增加注释

--- spring-boot/src/main/java/com/liuzh/Application.java
+++ spring-boot/src/main/java/com/liuzh/Application.java
@@ -1,17 +1,17 @@
 package com.liuzh;
 
-import com.liuzh.core.utils.SpringContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 /**
+ * 启动类
+ * 此类要在外层目录 否则Spring的依赖注入会失败
+ *
  * Created by zhaohong on 2017/10/18.
  */
 @SpringBootApplication
-@ImportResource({"classpath:velocity.xml"})
-//@Import(value = {SpringContextUtil.class})
+@ImportResource({"classpath:velocity.xml"}) // 引入velocity模版
 public class Application {
 
     public static void main(String[] args){
